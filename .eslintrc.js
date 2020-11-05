b1ec4ee0f7dd7c8c74b44d40e190958ec9bf3ee8
Andrew Huth <ahuth@chanzuckerberg.com>
2020-11-04 21:36:39 -0800

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['storybook-static/', 'build/'],
+  plugins: ['import', 'jest'],
+  rules: {},
+  settings: {
+    'import/extensions': ['.js', '.jsx'],
+  }
+};
