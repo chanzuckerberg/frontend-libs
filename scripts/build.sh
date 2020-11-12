1afa97cbc9d9b105b8c357af24f46e8dd92a47aa
Andrew Huth <ahuth@chanzuckerberg.com>
2020-11-12 09:53:28 -0800

    Update build script comment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build everything with tsc, including test files. We only really want non-test files. But I have
-# not have a good way to not compile test files to JS while still type-checking them.
+# not found a good way to not compile test files to JS while still type-checking them.
 #
 # If we find a good way to do that, we should be able to remove this script with a simple build
 # command.
