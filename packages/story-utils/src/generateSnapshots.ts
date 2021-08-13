7982cac42e0abc069102760055c471d5d34ceb6b
Shlomi Nissan <snissan@chanzuckerberg.com>
2021-08-13 05:32:41 -0700

    Invoke wait() after the component is rendered

--- packages/story-utils/src/generateSnapshots.ts
+++ packages/story-utils/src/generateSnapshots.ts
@@ -41,6 +41,12 @@ export default function generateSnapshots<
 
     test(`${storyName} story renders snapshot`, async () => {
       const view = render(prepareStory(story.storyFn, argOverrides));
+
+      // When components that include Apollo's useQuery are rendered we need
+      // to await an act that pushes the test to the end of the event loop.
+      // https://trojanowski.dev/apollo-hooks-testing-without-act-warnings/ 
+      await wait();
+      
       expect(await getSnapshot(view)).toMatchSnapshot();
     });
   }
