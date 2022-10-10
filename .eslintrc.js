c04810eff251f1f08a9c851582aa5649c79b5d45
Andrew Huth <ahuth@chanzuckerberg.com>
2022-10-10 10:45:53 -0400

    Use shared lint packages for linting this repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   env: {
     browser: true,
     es2021: true,
@@ -6,23 +11,12 @@ module.exports = {
     node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
+    '@chanzuckerberg/eslint-config-edu-js',
+    '@chanzuckerberg/eslint-config-edu-ts',
     'plugin:import/warnings',
-    'plugin:import/typescript',
     'plugin:prettier/recommended',
     'plugin:testing-library/react',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
   ignorePatterns: ['storybook-static/', 'build/'],
-  plugins: ['@typescript-eslint', 'testing-library', 'import', 'jest'],
-  rules: {
-    '@typescript-eslint/no-empty-function': 'off',
-    'import/order': ['error', { alphabetize: { order: 'asc' } }],
-  },
+  plugins: ['testing-library', 'jest'],
 };
