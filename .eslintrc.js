f4f52945204643183cab483504c096a10d11d6b0
Andrew Huth <ahuth@chanzuckerberg.com>
2022-10-10 20:21:10 -0400

    Turn off import/no-extraneous-dependencies, unfortunately

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,4 +19,10 @@ module.exports = {
   ],
   ignorePatterns: ['storybook-static/', 'build/'],
   plugins: ['testing-library', 'jest'],
+  rules: {
+    // This lint rule is extremely useful, but does not seem to work in this repo... not sure why 😬
+    // Is it because of the monorepo structure, and eslint can't tell that dev deps at the top level
+    // should be importable by packages?
+    'import/no-extraneous-dependencies': 'off',
+  },
 };
