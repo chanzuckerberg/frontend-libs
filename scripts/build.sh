f9c309b1a035e4660eeca4dfd0d2186033878ba4
Andrew Huth <ahuth@chanzuckerberg.com>
2022-10-19 09:20:14 -0400

    Add more detail to comment explaining the build.sh script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Build everything with tsc, including test files. We only really want non-test files. But I have
-# not found a good way to not compile test files to JS while still type-checking them.
+# Build everything with tsc, including test files. We only really want non-test files, but I have
+# not found a good way build project references while excluding tests files. In particular the
+# `--project` and `--build` options can't be used together.
 #
 # If we find a good way to do that, we should be able to remove this script with a simple build
 # command.
@@ -10,6 +11,7 @@ yarn build:ts
 # Remove test files and their declarations.
 npx rimraf \
   'packages/*/build/**/__tests__' \
-  'packages/*/build/**/*.test.*' \
+  'packages/*/build/**/*.stories.*' \
   'packages/*/build/**/*.spec.*' \
-  'packages/*/build/**/*.stories.*'
+  'packages/*/build/**/*.test.*' \
+  'packages/*/build/**/*.ts.snap'
