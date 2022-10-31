2595c1fdf92c7cd7b5a748dbf419a953ec3d8514
Andrew Huth <ahuth@chanzuckerberg.com>
2022-10-31 09:25:53 -0400

    Use msw-storybook-addon instead of our own custom integration

--- packages/cra-template-edu/template/.storybook/preview.js
+++ packages/cra-template-edu/template/.storybook/preview.js
@@ -1,4 +1,9 @@
+import {initialize as initializeMSW, mswDecorator} from 'msw-storybook-addon';
 import '../src/index.css';
+
+initializeMSW();
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- packages/cra-template-edu/template/src/components/SFWeather/SFWeather.stories.tsx
+++ packages/cra-template-edu/template/src/components/SFWeather/SFWeather.stories.tsx
@@ -1,32 +1,45 @@
 import type {StoryObj} from '@storybook/react';
 import {within} from '@storybook/testing-library';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {rest} from 'msw';
-import WithMockRequests from '../../testing/WithMockRequests';
 import SFWeather from './SFWeather';
 
 export default {
   component: SFWeather,
 };
 
-const handlers = [
-  rest.get('https://api.open-meteo.com/v1/forecast', (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({current_weather: {temperature: 66.6}}),
-    );
-  }),
-];
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export const Standard: StoryObj = {
   render: () => (
-    <WithMockRequests requestHandlers={handlers}>
+    <QueryClientProvider client={queryClient}>
       <SFWeather />
-    </WithMockRequests>
+    </QueryClientProvider>
   ),
   play: async ({canvasElement}) => {
     const canvas = within(canvasElement);
     // Wait for the API request to finish before doing anything else (such as a11y testing, visual
     // regression testing, etc).
     await canvas.findByText('Current San Francisco temp: 66.6');
+  },
+  parameters: {
+    // Mock out API requests by specifying MSW handlers. See https://mswjs.io/docs/.
+    msw: {
+      handlers: [
+        rest.get('https://api.open-meteo.com/v1/forecast', (req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({current_weather: {temperature: 66.6}}),
+          );
+        }),
+      ],
+    },
   },
 };
