42a4932573e30d86907a89758f0ba175a37d9646
Andrew Huth <ahuth@chanzuckerberg.com>
2022-11-01 08:54:26 -0400

    Use async function in example API call

--- packages/cra-template-edu/CHANGELOG.md
+++ packages/cra-template-edu/CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased
 
+- [fix] Use an async function in example API call
+
 ## 1.0.2 (2022-11-01)
 
 - [fix] Fix component name (the About component's function was named App...)
--- packages/cra-template-edu/template/src/api/weather.ts
+++ packages/cra-template-edu/template/src/api/weather.ts
@@ -4,17 +4,15 @@ type WeatherResponse = {
   };
 };
 
-export function getTemp(lat: number, long: number) {
-  return fetch(
+export async function getTemp(lat: number, long: number) {
+  const response = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&temperature_unit=fahrenheit&current_weather=true`,
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((json: WeatherResponse) => {
-      return json.current_weather.temperature;
-    });
+  );
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const json: WeatherResponse = await response.json();
+  return json.current_weather.temperature;
 }
