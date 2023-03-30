2326634249fa4aaf269be1962709fa3947f50813
Andrew Huth <ahuth@chanzuckerberg.com>
2023-03-30 08:52:19 -0400

    Split out useStableRef into its own file

--- packages/remix-utils/src/usePageChange.ts
+++ packages/remix-utils/src/usePageChange.ts
@@ -1,5 +1,6 @@
 import {useLocation} from '@remix-run/react';
 import {useEffect, useRef} from 'react';
+import useStableRef from './useStableRef';
 
 /**
  * Execute a callback when the page changes.
@@ -15,29 +16,4 @@ export default function usePageChange(
     callbackRef.current(location.pathname, prevPathRef.current);
     prevPathRef.current = location.pathname;
   }, [callbackRef, location.pathname]);
-}
-
-/**
- * Create a ref pointing to a value, and keep the ref updated with the latest value.
- *
- * Think of this ref like a pointer, or a Ruby instance variable.
- *
- * Basically a convenience wrapper combining a `useRef` and a `useEffect`. Most useful for
- * referencing something mutable in a `useEffect` without worrying about tearing the effect down
- * and setting it back up.
- *
- * @example
- * const callbackRef = useStableRef(callback);
- *
- * useEffect(() => {
- *   // Use your ref
- *   callbackRef.current();
- * }, [callbackRef]);
- */
-function useStableRef<T>(val: T) {
-  const ref = useRef(val);
-  useEffect(() => {
-    ref.current = val;
-  }, [val]);
-  return ref;
 }
